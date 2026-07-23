e5d1747a340fa9241dad526bacbf95f67509fde4
Daksh Dobhal <d.dobhal03@gmail.com>
2026-07-23 21:46:26 +0530

    Add logout and /etc folder | 23-07-2026

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -7,8 +7,13 @@
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
-hl.on("hyprland.start", function () 
+hl.on("hyprland.start", function ()
+  -- GNOME KEYRING CONFS
+  hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+  hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")  
+
   hl.exec_cmd("~/.config/waybar/scripts/launch.sh")
   hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("nm-applet --indicator")
   hl.exec_cmd("swaync")
 end)
--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Then open `vim` and run `:PluginInstall` to actually install wal.vim (declared i
 Make sure to use `gvim` to have system wide clipboard access in vim
 
 yay -S pixie-sddm-git
-The files under the `usr` dir must be placed under the `/usr` dir of the computer and not in `home` dir
+The files under the `usr` dir must be placed under the `/usr` dir of the computer and not in `home` dir. The same goes for files under `etc`
